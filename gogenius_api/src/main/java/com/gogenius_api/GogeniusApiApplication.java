6dafe12d3b678ea8d4407a1a6463eddecda0159c
nouhaila2001204 <blachenouhaila@gmail.com>
2025-12-30 00:11:45 +0100

    Ajout complet du projet Spring Boot gogenius_api

---
+++ gogenius_api/src/main/java/com/gogenius_api/GogeniusApiApplication.java
@@ -0,0 +1,13 @@
+package com.gogenius_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogeniusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogeniusApiApplication.class, args);
+	}
+
+}
---
+++ gogenius_api/src/test/java/com/gogenius_api/GogeniusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogenius_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogeniusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
