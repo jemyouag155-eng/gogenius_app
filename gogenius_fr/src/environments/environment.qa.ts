cf6d3e9d65e6880664118c3a3dccd7540d84df48
yassine-taleb-dev <dev.prognft@gmail.com>
2026-01-01 00:08:08 +0100

    add login

---
+++ gogenius_fr/src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    API_URL: 'http://qa-api-ms.gogeniusframework.com/api/v1',
+    SERVER_URL: 'http://qa-web-ms.gogeniusframework.com',
+    OAUTH_REDIRECT_URL: 'http://qa-app-ms.gogeniusframework.com',
+    OAUTH_CLIENT_ID: 'client-super-qa',
+    OAUTH_CLIENT_SECRET: 'secret'
+  };
---
+++ gogenius_fr/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    API_URL: 'http://localhost:8443/api/v1',
+    SERVER_URL: 'http://localhost:8443',
+    OAUTH_REDIRECT_URL: 'http://localhost:3000/dashboard',
+    OAUTH_CLIENT_ID: 'client-super',
+    OAUTH_CLIENT_SECRET: 'secret'
+  };
